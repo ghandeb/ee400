04f25ac8677f35a3bc10026df9e2fa7e36fbbc44
ghandeb <hande.bayazit@metu.edu.tr>
2017-07-06 23:07:37 +0300

    :(
    iteration yapan bilgisayarı soba olarak kullanmak

--- alternating.m
+++ alternating.m
@@ -1,34 +1,42 @@
 clc
 close all
 clear all
+% % 
+% % %level 1 severity
 
-%level 1 severity
+time=0:0.001:120
 
-time=0:0.0000001:0.012
-time1=0:0.0000001:0.006
-time2=0.0060001:0.0000001:0.012
-trev=0.0060001:-0.0000001:0.0000001
-
-trev=transpose(trev)
 time=transpose(time)
-time1=transpose(time1)
-time2=transpose(time2)
+
 
 num=numel(time)
 freq=zeros(num,1)
+% % 
+% % freq1=50+(25000-50)/60.*time1
+% % freq2=50000-(25000-50)/60.*time2
+% % 
+% % freq=cat(1,freq1,freq2)
+% % 
+% % plot(time,freq)
+% % %vpp=1 V
+% % t=-0.0006:0.00000001:0.0006
+% % t=transpose(t)
 
-freq1=50+(25000-50)/0.006.*time1
-freq2=50000-(25000-50)/0.006.*trev
+for k=1:60001
+    freq(k,1)=0.005+(2.5-0.005)/60*time(k,1)
+end
+    freq(60002,1)=2.5
+    
+for m=60003:num
+        freq(m,1)=-1*(2.5-0.005)/60*(time(m,1)-60)+0.005
+end
+    
 
-freq=cat(1,freq1,freq2)
-
-%vpp=1 V
-
-wf=0.5*sin(2*pi*time.*freq)
-
-
-
-plot(wf)
+ wf=0.5*sin(2*pi*time.*freq-pi)
+figure
+ plot(time,wf)
+ figure
 plot(time,freq)
+
 
 
